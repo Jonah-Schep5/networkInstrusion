ab57e7503f332081f80bc361adb2422bd076975a
zowney <muhoberacofficial@gmail.com>
2025-11-16 21:24:28 -0500

    Rearragned and made cleaner

--- setup_rapids_ads5984.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# RAPIDS Environment Setup Script for ads_5984
-# Source this script before starting Jupyter or running Python with RAPIDS
-
-export LD_LIBRARY_PATH="/home/jonahs23/.local/lib/python3.10/site-packages/libcuml/lib64:/home/jonahs23/.local/lib/python3.10/site-packages/libcudf/lib64:/home/jonahs23/.local/lib/python3.10/site-packages/libraft/lib64:/home/jonahs23/.local/lib/python3.10/site-packages/librmm/lib64:/home/jonahs23/.local/lib/python3.10/site-packages/libucxx/lib64:/opt/anaconda3/lib/python3.12/site-packages/nvidia/nccl/lib:$LD_LIBRARY_PATH"
-
-echo "RAPIDS environment configured for ads_5984!"
-echo "LD_LIBRARY_PATH has been set for cuML, cuDF, and CuPy."
